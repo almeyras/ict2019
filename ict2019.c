a6634b4ce4a3a051127285ff652338a60ac158ae
almeyras <69123882+almeyras@users.noreply.github.com>
2021-03-30 23:19:57 +0200

    Update ict2019.c
    Cambios en regletas de entrada y de salida.

--- ict2019.c
+++ ict2019.c
@@ -4,6 +4,7 @@
 int plantas = 0;
 int viviendas = 0;
 char tipopares = 'a';
+float acometidasstdp = 0;
 int regletassalida = 0;
 int regletasentrada = 0;
 
@@ -13,7 +14,7 @@ int main(void) {
 	scanf("%i", &plantas);
 	printf("\nIntroduce numero total de viviendas (generalmente multiplica plantas · viviendas/planta): ");
 	scanf("%i", &viviendas);
-	printf("\n¿La distancia entre RITI/RITU y PAU más lejano MENOS de 100 metros (s/n)?");
+	printf("\n¿La distancia entre RITI/RITU y PAU más lejano MENOS de 100 metros (s/n)? ");
 	scanf(" %c", &tipopares);
 	
 	printf("\n\n- Numero de PAUs = %i", viviendas);
@@ -24,26 +25,28 @@ int main(void) {
 	// Pares o pares trenzados
 	if (tipopares == 's') // pares trenzados
 		{
-			printf("\n- Pares trenzados cat6 o superior. Se necesitan %i cables (contando reserva de planta).", (int)ceil((viviendas+1)*1.2));
+			acometidasstdp = (int)ceil((viviendas+1)*1.2);			
+			printf("\n- STDP con pares trenzados cat6 o superior. Se necesitan %i cables (contando reserva de planta).", (int)acometidasstdp);
 			
-			regletassalida = (int)ceil((viviendas+1)*1.2);	
-			printf("\n  Patch panel de salida: %i tomas RJ-45.", (int)ceil((viviendas+1)*1.2));
+			regletassalida = (int)acometidasstdp;
+			printf("\n  Patch panel de salida: %i tomas RJ-45.", regletassalida);
 			if (viviendas+1 > 10) regletasentrada = (int)ceil(regletassalida*1.5);
 			if (viviendas+1 <= 10) regletasentrada = (int)(regletassalida*2);
 			printf("\n  Patch panel de entrada: %i tomas RJ-45.", regletasentrada);
 		}
 	if (tipopares == 'n') // pares 
 		{
-			printf("\n- Pares en topología ");
+			printf("\n- STDP con pares en topologia ");
 			if ((viviendas+1)*2 <= 30 ) printf("estrella.");
 			if ((viviendas+1)*2 > 30 ) printf("manguera multipar.");
-			printf(" Se necesitan %i pares (contando reserva de planta).",(int)ceil((viviendas+1)*2*1.2));
+			acometidasstdp = (int)ceil((viviendas+1)*2*1.2);
+			printf(" Se necesitan %i pares (contando reserva de planta).", (int)acometidasstdp);
 			
-			regletassalida = (int)ceil((viviendas+1)*2*1.2);
-			printf("\n  Regletas de salida: %i regletas krone de 10 pares.", (int)(ceil(regletassalida/10)));		
-			if (viviendas+1 > 10) regletasentrada = (int)ceil((ceil(regletassalida/10))*1.5);
-			if (viviendas+1 <= 10) regletasentrada = (int)(ceil(regletassalida/10))*2;
-			printf("\n  Patch panel de entrada: %i regletas krone de 10 pares.", regletasentrada);
+			regletassalida = (int)ceil(acometidasstdp/10);
+			printf("\n  Regletas de salida: %i regletas krone de 10 pares.", regletassalida);		
+			if (viviendas+1 > 10) regletasentrada = ceil(acometidasstdp*1.5/10);
+			if (viviendas+1 <= 10) regletasentrada = ceil(acometidasstdp*2/10);
+			printf("\n  Regletas de entrada: %i regletas krone de 10 pares.", regletasentrada);
 		}	
 		
 	// TBA (banda ancha coaxial)	
